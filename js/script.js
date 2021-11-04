952cd22c86113030c0cd4cf5eaca0d1cd3cc3d31
francesco <francescosanti91@gmail.com>
2021-11-04 14:36:44 +0100

    passi logici

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Passi logici:
+
+// creo un array di oggetti
+    // ogni oggetto contiene le informazioni per ciascun membro del team (nome, ruolo, percorso dell'immagine)
+// scorro l'array di oggetti
+    // con le informazioni dell'oggetto, stampo in pagina la sua card, replicando la struttura html di partenza
+        // creo un div team-card
+        // creo un div card-image
+            // al suo interno inserisco l'immagine col percorso e il nome presi dall'oggetto
+        // creo un div card-text
+            // al suo interno inserisco nome e ruolo presi dall'oggetto
+        // inserisco card-image e card-text all'interno di team-card
+        // stampo in pagina il div team-card con tutti i suoi elementi
+// al click del pulsante add
+    // salvo gli input inseriti dall'utente in delle variabili
+    // creo un nuovo oggetto con gli input inseriti dall'utente
+    // aggiungo il nuovo oggetto all'array dei membri del team
+    // aggiorno ciò che viene visualizzato in pagina, così da poter vedere anche il nuovo membro aggiunto
+
